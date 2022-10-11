cfb967f83fb44c0544d2a05b45f629c3086f4e4c
AlanVerdejo <a.verdejo2@alumnos.santotomas.cl>
2022-10-10 23:30:32 -0300

    Send

--- app/src/main/java/com/accounthub/v1/MainActivity.java
+++ app/src/main/java/com/accounthub/v1/MainActivity.java
@@ -2,13 +2,31 @@ package com.accounthub.v1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnEntrar=findViewById(R.id.buttonIn);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent( packageContext: MainActivity.this, Loading.class);
+                startActivity(i);
+
+            }
+        });
     }
+
 }
